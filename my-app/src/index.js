f094073fc283fb858ce74f8e95b0253a2bc2a80a
Vadim <88936621+zemk@users.noreply.github.com>
2021-11-21 21:24:57 +0300

    src

---
+++ my-app/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import './index.css';
+import App from './components/app';
+import reportWebVitals from './reportWebVitals';
+
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
